4985b179d81c06c691b2db20609a65b1a28a0f95
Alberto <albertobaccega02@gmail.com>
2019-04-10 13:51:26 +0200

    PRIMO LAVORO
    Inizio dei lavori

---
+++ sketch_apr10a.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+#define Pulsante1
+#define Pulsante2
+#define Pulsante3
+#define Pulsante4
+#define Pulsante5
+
+int Vite;
+int Record;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Vite = 5;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
