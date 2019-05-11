7b4b9f686c0097758ea2f6cb1402786ee28016c9
Alberto <albertobaccega02@gmail.com>
2019-05-11 09:28:41 +0200

    QUASI TERMINE LAVORI / ULTIMI RITOCCHI

--- sketch_apr10a/sketch_apr10a.ino
+++ sketch_apr10a/sketch_apr10a.ino
@@ -13,9 +13,9 @@ int Record;
 int Jolly;
 int Penalty;
 int Random;
+int RandomSpeciali;
 
 bool Cliccato = false;
-
 
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 
@@ -33,7 +33,8 @@ void setup() {
   Partite = 0;
   Record = 0;
   Vite = 3;
-  Random = random(1, 5);
+  Random = random(1, 7);
+  RandomSpeciali = random(0, 5);
 
 }
 
@@ -44,37 +45,104 @@ void loop() {
 
 void PiGreco()
 {
-  while(digitalRead(PulsanteAccensione) == LOW){} 
+  while(digitalRead(PulsanteAccensione) == LOW) {} 
     lcd.setCursor(0, 0);
-    lcd.println( "VITE:" + String() + "PARTITE:" + String(Partite));
-    lcd.setCursor(0, 1);
+    lcd.println( "VITE:" + String(Vite) + "PARTITE:" + String(Partite));
     while(Vite > 0)
     {
       if(Random == 1)
       {
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.println("π");
         while(digitalRead(PulsanteA) == LOW) {}
         Record++;
+        CalcolaPartite(Partite);
+        if(digitalRead(PulsanteB) == HIGH || digitalRead(PulsanteC) == HIGH || digitalRead(PulsanteD) == HIGH || digitalRead(PulsanteE) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
+      }
+      else if(Random == 2)
+      {
+        lcd.setCursor(0, 2);
+        lcd.println("π");
+        while(digitalRead(PulsanteB) == LOW) {}
+        Record++;
+        CalcolaPartite(Partite);
+        if(digitalRead(PulsanteA) == HIGH || digitalRead(PulsanteC) == HIGH || digitalRead(PulsanteD) == HIGH || digitalRead(PulsanteE) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
+      }
+      else if(Random == 3)
+      {
+        lcd.setCursor(0, 3);
+        lcd.println("π");
+        while(digitalRead(PulsanteC) == LOW) {}
+        Record++;
+        CalcolaPartite(Partite);
+        if(digitalRead(PulsanteB) == HIGH || digitalRead(PulsanteA) == HIGH || digitalRead(PulsanteD) == HIGH || digitalRead(PulsanteE) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
+      }
+      else if(Random == 4)
+      {
+        lcd.setCursor(0, 4);
+        lcd.println("π");
+        while(digitalRead(PulsanteD) == LOW) {}
+        Record++;
+        CalcolaPartite(Partite);
+        if(digitalRead(PulsanteB) == HIGH || digitalRead(PulsanteC) == HIGH || digitalRead(PulsanteA) == HIGH || digitalRead(PulsanteE) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
+      }
+      else if(Random == 5)
+      {
+        lcd.setCursor(0, 5);
+        lcd.println("π");
+        while(digitalRead(PulsanteE) == LOW) {}
+        Record++;
+        CalcolaPartite(Partite);
+        if(digitalRead(PulsanteB) == HIGH || digitalRead(PulsanteC) == HIGH || digitalRead(PulsanteD) == HIGH || digitalRead(PulsanteA) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
+      }
+      else if(Random == 6)
+      {
+        lcd.setCursor(RandomSpeciali, 0);
+        lcd.println("J");
+        if(RandomSpeciali == 0)
+        {
+          
+        }
+        if(digitalRead(PulsanteB) == HIGH || digitalRead(PulsanteC) == HIGH || digitalRead(PulsanteD) == HIGH || digitalRead(PulsanteA) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
+      }
+      else if(Random == 7)
+      {
+        lcd.setCursor(0, random(0, 5));
+        lcd.println("P");
+        while(digitalRead(PulsanteE) == LOW) {}
+        Record++;
+        CalcolaPartite(Partite);
+        if(digitalRead(PulsanteB) == HIGH || digitalRead(PulsanteC) == HIGH || digitalRead(PulsanteD) == HIGH || digitalRead(PulsanteA) == HIGH)
+        {
+          Vite--;
+          CalcolaPartite(Partite);
+        }
       }
     }
-    
-  
-}
-
-void CalcolaVite(int Vite)
-{
-  while(digitalRead(PulsanteAccensione) == LOW) {}
-  
-}
-
-void CalcolaRecord(int Record)
-{
-  while(digitalRead(PulsanteAccensione) == LOW) {}
-  while(Cliccato == false)
-  {
-    
-  }
 }
 
 void CalcolaPartite(int Partite)
